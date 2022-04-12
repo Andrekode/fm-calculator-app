bfda0aaf4035d77eb2a0b43b8ceb5c579fdba385
Andrekode <93523910+Andrekode@users.noreply.github.com>
2022-04-12 12:17:21 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from 'react'
 
+import { StyledButton } from './Components/Button/Button.style'
+import { Container } from './Container.style'
+import Button from './Components/Button'
+
 export default function App() {
+
+    const buttonValues = [
+        7,8,9,'DEL',
+        4,5,6,"+",
+        1,2,3,"-",
+        ".",0,"/","x",
+        "RESET","="
+    ]
+
+    const buttonValuesob = {
+        buttonText: [1,2,3,"-",".",0,"/","x","RESET","="],
+
+        
+    }
+
+   
+
+    const buttons = buttonValues.map((btn, i) => {
+        return (
+            <StyledButton value={btn} key={i}  backgroundColor='red'> </StyledButton>
+        )
+    })
+
+    
     return (
-        <h1>test</h1>
+        <Container>
+           {buttons}
+        </Container>
+            
+        
     )
 }
