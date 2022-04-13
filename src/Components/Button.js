89794b81d8511f70ea78f58032198239b4716f61
Andrekode <93523910+Andrekode@users.noreply.github.com>
2022-04-13 11:48:06 +0200

    refactor buttons props

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,5 +2,13 @@ import React from 'react'
 
 
 export default function Button({className, value, key, handleClick, e}) {
-    return <button  key={key} className={className} onClick={(e) => handleClick(e)}>{value}</button>
+    return (
+        <button  
+            key={key} 
+            className={className} 
+            onClick={(e) => handleClick(e)}>
+
+            {value}
+        </button>
+    )
 }
--- src/Components/Button/Button.style.js
+++ src/Components/Button/Button.style.js
@@ -18,25 +18,21 @@ export const StyledButton = styled(Button)`
     
     
     border: 0;
-    
-    ${props => props.value === 'DEL' && `background-color: #637097;`}
-    ${props => props.value === 'DEL' && `color: #ffff;`}
-    ${props => props.value === 'DEL' && `box-shadow: 0 5px  #404e72;`}
-    ${props => props.value === 'DEL' && `font-size: 1rem;`}
-    ${props => props.value === 'DEL' && `font-weight: 700;`}
 
-    ${props => props.value === 'RESET' && `background-color: #637097;`}
-    ${props => props.value === 'RESET' && `color: #ffff;`}
-    ${props => props.value === 'RESET' && `box-shadow: 0 5px  #404e72;`}
-    ${props => props.value === 'RESET' && `font-size: 1rem;`}
-    ${props => props.value === 'RESET' && `font-weight: 700;`}
-    
+    /* invidual button styles DEL RESET and = */
+
+    ${props => (props.value === 'DEL' || props.value === 'RESET') && `background-color: #637097;`}
+    ${props => (props.value === 'DEL' || props.value === 'RESET' || props.value === '=') && `color: #ffff;`}
+    ${props => (props.value === 'DEL' || props.value === 'RESET') && `box-shadow: 0 5px  #404e72;`}
+    ${props => (props.value === 'DEL' || props.value === 'RESET' || props.value === '=') && `font-size: 1rem;`}
+    ${props => (props.value === 'DEL' || props.value === 'RESET' || props.value === '=')  && `font-weight: 700;`}
+
     ${props => props.value === '=' && `background-color: #d03f2f;`}
-    ${props => props.value === '=' && `color: #ffff;`}
     ${props => props.value === '=' && `box-shadow: 0 5px #93261a;`}
-    ${props => props.value === '=' && `font-size: 1rem;`}
-    ${props => props.value === '=' && `font-weight: 700;`}
     
+  
+    /* Grid spans RESET and = */
+
     ${props => props.value === "RESET" && `grid-column: 1 / span 2 `}
     ${props => props.value === '=' && `grid-column: 3 / span 2 `}
     
